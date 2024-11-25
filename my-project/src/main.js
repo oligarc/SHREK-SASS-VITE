f8acf27f371a45cbd44606609f2e9d3d2e9a74be
oligarc <oliver25399@gmail.com>
2024-11-25 18:56:42 +0100

    vite config

---
+++ my-project/src/main.js
@@ -0,0 +1,13 @@
+import "./style.css";
+import "./styles/main.scss";
+document.querySelector("#app").innerHTML = `
+  <div>
+    <h1>Hello Vite!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on my balls
+    </p>
+  </div>
+`;
