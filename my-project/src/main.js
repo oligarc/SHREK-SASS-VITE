ff060e1fd4ce3225b28209f3a73af2fb2f4f8699
oligarc <oliver25399@gmail.com>
2024-11-26 12:44:24 +0100

    seguir migrando

--- my-project/src/main.js
+++ my-project/src/main.js
@@ -1,13 +1,1 @@
-import "./style.css";
 import "./styles/main.scss";
-document.querySelector("#app").innerHTML = `
-  <div>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on my balls
-    </p>
-  </div>
-`;
