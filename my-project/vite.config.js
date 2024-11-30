3ef75835ee03a14882a2a609a5dd9b171d458518
oligarc <oliver25399@gmail.com>
2024-11-30 16:18:20 +0100

    githubpages

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   root: "./src", // Cambia la raíz del proyecto a "src"
   build: {
-    outDir: "../docs", // Los archivos construidos irán a la carpeta "docs"
+    outDir: "../docs", // Los archivos construidos irán a la carpeta "docs" (GitHub Pages usa esta carpeta automáticamente)
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"), // Página principal
+        index2: resolve(__dirname, "src/index2.html"), // Otra página (index2.html)
+      },
+    },
   },
+  base: "./", // Usa rutas relativas para mayor compatibilidad en GitHub Pages
 });
